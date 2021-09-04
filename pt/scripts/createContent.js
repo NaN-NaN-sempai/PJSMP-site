4a60dbe73e31ad8d91153071468a9137ed39e549
Luís Henrique de Almeida <lubizinho-2001@hotmail.com>
2021-09-03 22:50:11 -0300

    Método de Newton-Raphson colocado em estudos

--- pt/scripts/createContent.js
+++ pt/scripts/createContent.js
@@ -37,17 +37,6 @@ createContent({
     id: "projects",
     color: "white",
     solidBg: "linear-gradient(to right, white 49.8%, black 49.8%, black 50.2%, white 50.2%), linear-gradient(to top, white 49.8%, black 49.8%, black 50.2%, white 50.2%)",
-    title: "Método de Newton-Raphson", 
-    type: ["Web App", "Previa"],
-    desc: 'Calcula e mostra a raiz de funções usando o método de Newton-Raphson usando o projeto "Visualizador de Funções".',
-    onClick: "./projetos/metodoDeNewton/"
-});
-
-
-createContent({
-    id: "projects",
-    color: "white",
-    solidBg: "linear-gradient(to right, white 49.8%, black 49.8%, black 50.2%, white 50.2%), linear-gradient(to top, white 49.8%, black 49.8%, black 50.2%, white 50.2%)",
     title: "Visualizador de Funções", 
     type: ["Web App", "Previa"],
     desc: "Visualizador de funções usando Javascript e HTML.",
@@ -72,14 +61,25 @@ createContainer({
     id: "studies"
 });
 
+
 createContent({
+    id: "studies",
+    color: "white",
+    solidBg: "linear-gradient(to right, white 49.8%, black 49.8%, black 50.2%, white 50.2%), linear-gradient(to top, white 49.8%, black 49.8%, black 50.2%, white 50.2%)",
+    title: "Método de Newton-Raphson", 
+    type: ["Web App", "Previa"],
+    desc: 'Calcula e mostra a raiz de funções usando o método de Newton-Raphson, o visualizador é uma copia do projeto "Visualizador de Funções" encontrado na aba "Projetos".<br>Requisitado pela matéria <i>Matemática Aplicada</i> da minha faculdade.',
+    onClick: "./projetos/metodoDeNewton/"
+});
+
+/* createContent({
     id: "studies",
     color: "white",
     solidBg: "transparent",
     title: "Sem tópicos",
     desc: "Sem tópicos por enquanto, mas futuros tópicos irão aparecer aqui!",
     onClick: ()=>{}
-});
+}); */
 
 
 // ############################################## MORE #############################################
